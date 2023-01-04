5ad4bae16651129bd9f466cf5c4dd15c7c5a8491
aacitelli <aacitelli@PDD-NB-0205.dgsgroup.it>
2023-01-04 11:05:29 +0100

    Merge branch 'PAGOPA-604-mapping-response-PM' of https://github.com/pagopa/pagopa-biz-events-datastore into PAGOPA-604-mapping-response-PM

--- src/main/java/it/gov/pagopa/bizeventsdatastore/entity/TransactionDetails.java
+++ src/main/java/it/gov/pagopa/bizeventsdatastore/entity/TransactionDetails.java
@@ -12,6 +12,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 public class TransactionDetails {
+	private String origin;
 	private User user;
 	private Transaction transaction;
 	private WalletItem wallet;
