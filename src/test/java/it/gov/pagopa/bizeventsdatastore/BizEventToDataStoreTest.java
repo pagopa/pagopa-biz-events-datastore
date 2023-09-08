46054d746e6ed1d3a12d48e083ccc810d78370e8
alessio-acitelli <97529051+alessio-acitelli@users.noreply.github.com>
2023-09-08 15:06:57 +0200

    feat: [PAGOPA-1085] duplicated bizevent (#40)
    * [PAGOPA-1085] duplicated bizevent: added cache redis
    
    * rebase
    
    ---------
    
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>
    Co-authored-by: aacitelli <aacitelli@PDD-NB-0205.dgsgroup.it>

--- src/test/java/it/gov/pagopa/bizeventsdatastore/BizEventToDataStoreTest.java
+++ src/test/java/it/gov/pagopa/bizeventsdatastore/BizEventToDataStoreTest.java
@@ -1,6 +1,8 @@
 package it.gov.pagopa.bizeventsdatastore;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -20,6 +22,8 @@ import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.OutputBinding;
 
 import it.gov.pagopa.bizeventsdatastore.entity.BizEvent;
+import it.gov.pagopa.bizeventsdatastore.entity.DebtorPosition;
+import it.gov.pagopa.bizeventsdatastore.entity.PaymentInfo;
 
 @ExtendWith(MockitoExtension.class)
 class BizEventToDataStoreTest {
@@ -36,12 +40,18 @@ class BizEventToDataStoreTest {
         Logger logger = Logger.getLogger("BizEventToDataStore-test-logger");
         when(context.getLogger()).thenReturn(logger);
         
+        PaymentInfo pi = PaymentInfo.builder().IUR("iur").build();
+        DebtorPosition dp = DebtorPosition.builder().iuv("iuv").build();
+        
         List<BizEvent> bizEvtMsg = new ArrayList<>();
-        bizEvtMsg.add (new BizEvent());
+        bizEvtMsg.add (BizEvent.builder().id("123").paymentInfo(pi).debtorPosition(dp).build());
         
         Map<String, Object>[] properties = new HashMap[1];
         @SuppressWarnings("unchecked")
         OutputBinding<List<BizEvent>> document = (OutputBinding<List<BizEvent>>)mock(OutputBinding.class);
+        
+        doReturn(null).when(function).findByBizEventId(anyString());
+        doReturn("OK").when(function).saveBizEventId(anyString());
 
         // test execution
         function.processBizEvent(bizEvtMsg, properties, document, context);
@@ -62,6 +72,28 @@ class BizEventToDataStoreTest {
         Map<String, Object>[] properties = new HashMap[0];
         @SuppressWarnings("unchecked")
         OutputBinding<List<BizEvent>> document = (OutputBinding<List<BizEvent>>)mock(OutputBinding.class);
+
+        // test execution
+        function.processBizEvent(bizEvtMsg, properties, document, context);
+
+        // test assertion -> this line means the call was successful
+        assertTrue(true);
+    }
+    
+    @Test
+    void runBizEventAlreadyInCache() {
+        // test precondition
+        Logger logger = Logger.getLogger("BizEventToDataStore-test-logger");
+        when(context.getLogger()).thenReturn(logger);
+        
+        List<BizEvent> bizEvtMsg = new ArrayList<>();
+        bizEvtMsg.add (BizEvent.builder().id("123").build());
+        
+        Map<String, Object>[] properties = new HashMap[1];
+        @SuppressWarnings("unchecked")
+        OutputBinding<List<BizEvent>> document = (OutputBinding<List<BizEvent>>)mock(OutputBinding.class);
+        
+        doReturn("123").when(function).findByBizEventId(anyString());
 
         // test execution
         function.processBizEvent(bizEvtMsg, properties, document, context);
