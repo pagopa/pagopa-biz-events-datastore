b3416a0114c7b41ee203212498732bc00a649a59
aacitelli <aacitelli@PDD-NB-0205.dgsgroup.it>
2024-09-03 15:25:24 +0200

    [PAGOPA-2063] added junit test

--- src/main/java/it/gov/pagopa/bizeventsdatastore/entity/enumeration/ServiceIdentifierType.java
+++ src/main/java/it/gov/pagopa/bizeventsdatastore/entity/enumeration/ServiceIdentifierType.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 /**
  * Enum for transaction origin
  */
-public enum OriginType {
+public enum ServiceIdentifierType {
     INTERNAL, PM, NDP001PROD , NDP002PROD, NDP003PROD, UNKNOWN;
 
-    public static boolean isValidOrigin(String origin) {
+    public static boolean isValidServiceIdentifier(String origin) {
         return Arrays.stream(values()).anyMatch(it -> it.name().equalsIgnoreCase(origin));
     }
 }
