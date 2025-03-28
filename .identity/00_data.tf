9df66c48a32d92372b33f82514d0f00149209842
Pasquale Spica <36746022+pasqualespica@users.noreply.github.com>
2025-03-28 14:58:22 +0100

    chore: Add new wl (#94)
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -22,6 +22,22 @@ data "azurerm_key_vault" "key_vault" {
   resource_group_name = "pagopa-${var.env_short}-sec-rg"
 }
 
+
+data "azurerm_user_assigned_identity" "identity_cd" {
+  name                = "${local.product}-${local.domain}-job-01-github-cd-identity"
+  resource_group_name = "${local.product}-identity-rg"
+}
+
+data "azurerm_user_assigned_identity" "identity_cd_01"{
+  name = "${local.prefix}-${var.env_short}-${local.domain}-job-01-github-cd-identity"
+  resource_group_name = "${local.prefix}-${var.env_short}-identity-rg"
+}
+
+# data "azurerm_user_assigned_identity" "identity_pr_01" {
+#   name                = "${local.prefix}-${var.env_short}-${local.domain}-01-pr-github-cd-identity"
+#   resource_group_name = "${local.prefix}-${var.env_short}-identity-rg"
+# }
+
 data "azurerm_key_vault" "domain_key_vault" {
   name                = "pagopa-${var.env_short}-${local.domain}-kv"
   resource_group_name = "pagopa-${var.env_short}-${local.domain}-sec-rg"
@@ -36,9 +52,12 @@ data "azurerm_key_vault_secret" "key_vault_sonar" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
-data "azurerm_key_vault_secret" "key_vault_bot_token" {
+data "azurerm_key_vault_secret" "key_vault_bot_cd_token" {
+  # name         = "pagopa-platform-domain-github-bot-cd-pat"
+  # key_vault_id = data.azurerm_key_vault.domain_key_vault.id
   name         = "bot-token-github"
   key_vault_id = data.azurerm_key_vault.key_vault.id
+  
 }
 
 data "azurerm_key_vault_secret" "key_vault_cucumber_token" {
@@ -46,7 +65,30 @@ data "azurerm_key_vault_secret" "key_vault_cucumber_token" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
+data "azurerm_user_assigned_identity" "workload_identity_clientid" {
+   name                = "bizevents-workload-identity"
+   resource_group_name = "pagopa-${var.env_short}-weu-${var.env}-aks-rg"
+}
+
 data "azurerm_key_vault_secret" "key_vault_integration_test_subkey" {
   name         = "integration-test-subkey"
   key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "key_vault_integration_cosmos_biz_key" {
+  count  = var.env_short != "p" ? 1 : 0
+  name = format("cosmos-%s-biz-key", var.env_short)
+  key_vault_id = data.azurerm_key_vault.domain_key_vault.id
+}
+
+data "azurerm_key_vault_secret" "key_vault_integration_ehub_tx_biz_key" {
+  count  = var.env_short != "p" ? 1 : 0
+  name = format("ehub-tx-%s-biz-key", var.env_short)
+  key_vault_id = data.azurerm_key_vault.domain_key_vault.id
+}
+
+data "azurerm_key_vault_secret" "key_vault_integration_ehub_biz_connection_string" {
+  count  = var.env_short != "p" ? 1 : 0
+  name = format("ehub-%s-biz-connection-string", var.env_short)
+  key_vault_id = data.azurerm_key_vault.domain_key_vault.id
 }
